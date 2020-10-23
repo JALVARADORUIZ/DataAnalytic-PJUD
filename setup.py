856047939fa200b4cbe9c7ef113f5747de3424b5
Jose Armando <iflexus@gmail.com>
2020-10-22 22:53:23 -0300

    creacion proyecto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Este proyecto busca analizar los datos abiertos del Poder Judicial de Chile.',
+    author='escuelita-chilota',
+    license='MIT',
+)
