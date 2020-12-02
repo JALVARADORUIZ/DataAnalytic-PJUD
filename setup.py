8da8ebfd658a1f5d39598d3a12be6c7f1e2c4226
Vera Sativa <hola@verasativa.com>
2020-12-02 18:44:50 -0300

    wip: clickify

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='pjud',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     version='0.1.0',
     description='Este proyecto busca analizar los datos abiertos del Poder Judicial de Chile.',
     author='escuelita-chilota',
     license='MIT',
+    entry_points={
+        'console_scripts': [
+            'pjud=pjud.cli:main',
+        ],
+    },
 )
